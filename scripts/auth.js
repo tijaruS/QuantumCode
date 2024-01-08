ab60daa4935198229b52389411e357fd992e4a81
tijaruS <138298666+tijaruS@users.noreply.github.com>
2024-01-08 10:42:49 +0530

    Add redirect to index.html after sign out

--- scripts/auth.js
+++ scripts/auth.js
@@ -146,6 +146,9 @@ if (
         console.log("signed out");
         showMenu();
       })
+      .then(() => {
+        window.location.href = "index.html";
+      })
       .catch((error) => {
         // An error happened.
       });
