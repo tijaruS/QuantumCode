2b3b7d24e76a06fadebbfe1f5e271db33a659e6d
tijaruS <138298666+tijaruS@users.noreply.github.com>
2024-01-11 01:24:47 +0530

    Fix display of user name when display name is null

--- scripts/stalklist.js
+++ scripts/stalklist.js
@@ -3,8 +3,15 @@ import { auth } from "./auth.js";
 auth.onAuthStateChanged(function (user) {
   if (user) {
     document.querySelector("#ifLoggedIn").style.display = "block";
-    document.getElementById("userName").textContent = user.displayName;
+    if (user.displayName === null) {
+      user.email = user.email.split("@")[0];
+      document.getElementById("userName").textContent = `${user.email}`;
+    } else {
+      document.getElementById("userName").textContent = user.displayName;
+    }
   } else {
     document.querySelector("#ifLoggedOut").style.display = "block";
   }
 });
+
+console.log(userId);
