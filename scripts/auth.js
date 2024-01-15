42b67cde556810fc8de33234139c479c6de6b7f5
tijaruS <138298666+tijaruS@users.noreply.github.com>
2024-01-15 22:28:16 +0530

    Add user notification modal and functionality

--- scripts/auth.js
+++ scripts/auth.js
@@ -419,6 +419,7 @@ function sendAddFriendReq(key) {
     name: localStorage.getItem("displayName"),
     photoURL: localStorage.getItem("userPhotoUrl"),
     date: new Date().toLocaleString(),
+    status: "pending",
   };
   push(ref(rdb, "notifications/"), notification);
 }
@@ -440,4 +441,73 @@ function notificationCount() {
   // if (document.querySelector("#notification") != null) {
   //   document.querySelector("#notification").innerHTML = count;
   // }
+}
+
+if (
+  window.location.pathname != "/signin.html" &&
+  window.location.pathname != "/signup.html" &&
+  window.location.pathname != "/stalklist.html" &&
+  window.location.pathname != "/contest.html" &&
+  window.location.pathname != "/search.html"
+) {
+  const notificationBtn = document.querySelector(".notificationBtn");
+  notificationBtn.addEventListener("click", (e) => {
+    populateNotificationList();
+  });
+}
+
+function populateNotificationList() {
+  let html = "";
+  const colDb = ref(rdb, "users/");
+  const notificationRef = ref(rdb, "notifications/");
+  const q = query(
+    notificationRef,
+    orderByChild("SendTo"),
+    equalTo(localStorage.getItem("userUid"))
+  );
+  // let notificationRef = firebase.database().ref("notifications");
+  onValue(q, (snapshot) => {
+    if (snapshot.hasChildren()) {
+      html = `<form class="d-flex mb-2" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      </form>`;
+    }
+
+    snapshot.forEach((data) => {
+      let use = data.val();
+      // console.log(use.email);
+
+      // console.log(snapshot.val());
+      // console.log(val);
+      // console.log(Object.values(snapshot.val()).length);
+
+      html += `
+      <div style='display:flex;align-items:center;margin-bottom:10px'>
+       <div class="userDP">
+                <img
+                  class="rounded-5"
+                  src="${use.photoURL}"
+                  alt=""
+                  height="40px"
+                />
+              </div>
+              <div class="userProfileName" style="margin-left:10px">
+                <h3 style="font-size: 20px; margin-top: 5px">${use.name}</h3>
+              </div>
+              <div class="ms-auto">
+              <button class="btn btn-success" ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill-check" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+</svg>Accept</button>
+              <button class="btn btn-danger" ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill-x" viewBox="0 0 16 16">
+  <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708"/>
+</svg>Reject</button>
+
+              </div>
+      </div>
+      `;
+      document.querySelector("#populateNotificationList").innerHTML = html;
+    });
+  });
 }
